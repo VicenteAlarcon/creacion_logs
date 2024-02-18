78c04f3e764c3ecbab04a06d69fb4c1899f6b8bd
Vicente Alarcon <alarcongonzalezvicente@gmail.com>
2024-02-18 17:13:59 +0100

    feat:"index"

--- logs/resources/views/videos/index.blade.php
+++ logs/resources/views/videos/index.blade.php
@@ -1,3 +1,27 @@
-<div>
-    <!-- The best way to take care of the future is to take care of the present moment. - Thich Nhat Hanh -->
-</div>
+@extends('layouts.layout')
+@section('contenido')
+
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Categoria</th>
+      <th scope="col">Duración</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($videos as $video)
+    <tr>
+      <th scope="row">{{$video->id}}</th>
+      <td>{{$video->name}}</td>
+      <td>{{$video->category}}</td>
+      <td>{{$video->duration}}</td>
+    </tr>
+  @endforeach
+  </tbody>
+</table>
+
+@endsection
+
+
