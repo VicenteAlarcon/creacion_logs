550a289294e29c3a7c742153ca52f7a12ba709d0
Vicente Alarcon <alarcongonzalezvicente@gmail.com>
2024-02-18 17:47:04 +0100

    fix:"error tipado"

--- logs/database/migrations/2024_02_18_163827_create_logs_table.php
+++ logs/database/migrations/2024_02_18_163827_create_logs_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('logs', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('descritpion');
+            $table->string('description');
             $table->timestamps();
         });
     }
