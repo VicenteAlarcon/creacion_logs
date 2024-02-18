3317366e5a489316e3719a4e169db5aa82ee90ae
Vicente Alarcon <alarcongonzalezvicente@gmail.com>
2024-02-18 16:33:40 +0100

    fix:"punto y coma"

--- logs/app/Http/Controllers/VideoController.php
+++ logs/app/Http/Controllers/VideoController.php
@@ -34,7 +34,7 @@ class VideoController extends Controller
      */
     public function create()
     {
-        return view('videos.create')
+        return view('videos.create');
     }
 
     /**
