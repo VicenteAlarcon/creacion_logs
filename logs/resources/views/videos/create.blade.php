187d8e7afcf209b59bea1c216fc83956eda9ce21
Vicente Alarcon <alarcongonzalezvicente@gmail.com>
2024-02-18 17:19:18 +0100

    feat:"creación"

--- logs/resources/views/videos/create.blade.php
+++ logs/resources/views/videos/create.blade.php
@@ -1,3 +1,28 @@
-<div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
-</div>
+@extends('layouts.layout')
+@section('contenido')
+
+<form action="{{route('videos.store)}}" method="post">
+    @csrf
+  <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Name</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputEmail3" name="name">
+    </div>
+  </div>
+  <div class="row mb-3">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">category</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputPassword3" name="category">
+    </div>
+  </div>
+
+  <div class="row mb-3">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">category</label>
+    <div class="col-sm-10">
+      <input type="number" class="form-control" id="inputPassword3" name="duration">
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Sign in</button>
+</form>
+
+@endsection
